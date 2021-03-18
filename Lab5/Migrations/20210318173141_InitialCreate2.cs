cf3e4c644b68f21e6dc6a570746be96c273fee11
Raphael Guerra <raphaelguerra88@gmail.com>
2021-03-18 14:22:59 -0400

    Add project files.

---
+++ Lab5/Migrations/20210318173141_InitialCreate2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab5.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Question",
+                table: "AnswerImage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Question",
+                table: "AnswerImage");
+        }
+    }
+}
---
+++ Lab5/Models/AnsweImage.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    public enum Question
+    {
+        Earth, Computer
+    }
+    public class AnswerImage
+    {
+        public int AnswerImageId
+        {
+            get; set;
+        }
+
+
+        [Required]
+        [DisplayName("File Name")]
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [Url]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public Question Question { get; set; }
+        [Display(Name = "Question")]
+        public string Answer
+        {
+            get
+            {
+                return Question == Question.Earth ? "Earth" : "Computer";
+            }
+        }
+    }
+}
